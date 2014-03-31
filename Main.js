e6c9e9be23922c32312bd257f98492ea6b812c08
Justin Yang <corodio8@gmail.com>
2014-03-31 12:44:17 -0700

    cleaned up testing code to finish part 3 of tutorial

--- Main.js
+++ Main.js
@@ -27,63 +27,6 @@ Main.prototype.spriteSheetLoaded = function() {
   this.scroller = new Scroller(this.stage)
   requestAnimFrame(this.update.bind(this));
 
-  this.pool = new WallSpritesPool();
-  this.wallSlices = [];
-
-};
-
-Main.prototype.generateTestWallSpan = function() {
-  var lookupTable = [
-    this.pool.borrowFrontEdge,
-    this.pool.borrowWindow,
-    this.pool.borrowDecoration,
-    this.pool.borrowStep,
-    this.pool.borrowWindow,
-    this.pool.borrowBackEdge
-  ];
-
-  var yPos = [
-    128, 
-    128,
-    128,
-    192,
-    192,
-    192
-  ];
-
-  for (var i = 0; i < lookupTable.length; i++) {
-    var func = lookupTable[i];
-
-    var sprite = func.call(this.pool);
-    sprite.position.x = 32 + (i * 64);
-    sprite.position.y = yPos[i];
-
-    this.wallSlices.push(sprite);
-
-    this.stage.addChild(sprite);
-  }
-};
-
-Main.prototype.clearTestWallSpan = function() {
-  var lookupTable = [
-    this.pool.returnFrontEdge,
-    this.pool.returnWindow,
-    this.pool.returnDecoration,
-    this.pool.returnStep,
-    this.pool.returnWindow,
-    this.pool.returnBackEdge
-  ];
-
-  for (var i = 0; i < lookupTable.length; i++) {
-    var func = lookupTable[i];
-    var sprite = this.wallSlices[i];
-
-    this.stage.removeChild(sprite);
-    func.call(this.pool, sprite);
-  }
-
-  this.wallSlices = [];
-
 };
 
 Main.prototype.borrowWallSprites = function(num) {
