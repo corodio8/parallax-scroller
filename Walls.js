81c1f882164eb827bd3e5c1d816a7864a37807f9
Justin Yang <corodio8@gmail.com>
2014-04-13 10:30:44 -0700

    Added test map, added code to borrow, display, update, return wall sections

--- Walls.js
+++ Walls.js
@@ -4,18 +4,80 @@ function Walls() {
   this.pool = new WallSpritesPool();
   this.createLookupTables();
 
-  console.log('Before borrowing window: ' + this.pool.windows.length);
-  var sprite = this.borrowWallSprite(SliceType.WINDOW);
-  this.addChild(sprite);
-  console.log('After borrowing window: ' + this.pool.windows.length);
+  this.slices = [];
+  this.createTestMap();
 
-  this.removeChild(sprite);
-  this.returnWallSprite(SliceType.WINDOW,sprite);
-  console.log('After returning window: ' + this.pool.windows.length);
+  this.viewportX = 0;
+  this.viewportSliceX = 0;
 }
 
 Walls.constructor = Walls;
 Walls.prototype = Object.create(PIXI.DisplayObjectContainer.prototype);
+
+Walls.VIEWPORT_WIDTH = 512;
+Walls.VIEWPORT_NUM_SLICES = Math.ceil(Walls.VIEWPORT_WIDTH/WallSlice.WIDTH) + 1;
+
+Walls.prototype.setViewportX = function(viewportX) {
+  this.viewportX = this.checkViewportXBounds(viewportX);
+
+  var prevViewportSliceX = this.viewportSliceX;
+  this.viewportSliceX = Math.floor(this.viewportX/WallSlice.WIDTH);
+
+  this.removeOldSlices(prevViewportSliceX);
+  this.addNewSlices();
+}
+
+Walls.prototype.addSlice = function(sliceType, y) {
+  var slice = new WallSlice(sliceType, y);
+  this.slices.push(slice);
+}
+
+Walls.prototype.checkViewportXBounds = function(viewportX) {
+  var maxViewportX = (this.slices.length - Walls.VIEWPORT_NUM_SLICES) * WallSlice.WIDTH;
+  if (viewportX < 0) {
+    viewportX = 0;
+  } else if (viewportX >= maxViewportX) {
+    viewportX = maxViewportX;
+  }
+  return viewportX
+};
+
+Walls.prototype.addNewSlices = function() {
+  var firstX = -(this.viewportX % WallSlice.WIDTH);
+  for (var i = this.viewportSliceX, sliceIndex = 0; 
+       i < this.viewportSliceX + Walls.VIEWPORT_NUM_SLICES; 
+       i++, sliceIndex++) {
+    var slice = this.slices[i];
+    if (slice.sprite == null && slice.type != SliceType.GAP) {
+      slice.sprite = this.borrowWallSprite(slice.type);
+
+      slice.sprite.position.x = firstX + (sliceIndex * WallSlice.WIDTH);
+      slice.sprite.position.y = slice.y;
+      
+      this.addChild(slice.sprite);
+
+    } else if (slice.sprite != null) {
+      slice.sprite.position.x = firstX + (sliceIndex * WallSlice.WIDTH);
+    }
+  }
+}
+
+Walls.prototype.removeOldSlices = function(prevViewportSliceX) {
+  var numOldSlices = this.viewportSliceX - prevViewportSliceX;
+  if (numOldSlices > Walls.VIEWPORT_NUM_SLICES) {
+    numOldSlices = Walls.VIEWPORT_NUM_SLICES;
+  }
+
+  for (var i = prevViewportSliceX; i < prevViewportSliceX + numOldSlices; i++) {
+    var slice = this.slices[i];
+    if (slice.sprite != null) {
+      this.returnWallSprite(slice.type, slice.sprite);
+      this.removeChild(slice.sprite);
+      slice.sprite = null;
+    }
+  }
+}
+
 
 Walls.prototype.createLookupTables = function() {
   this.borrowWallSpriteLookup = [];
@@ -39,4 +101,38 @@ Walls.prototype.borrowWallSprite = function(sliceType) {
 
 Walls.prototype.returnWallSprite = function(sliceType, sliceSprite) {
   return this.returnWallSpriteLookup[sliceType].call(this.pool, sliceSprite);
+}
+
+Walls.prototype.createTestWallSpan = function() {
+  this.addSlice(SliceType.FRONT, 192);
+  this.addSlice(SliceType.WINDOW, 192);
+  this.addSlice(SliceType.DECORATION, 192);
+  this.addSlice(SliceType.WINDOW, 192);
+  this.addSlice(SliceType.DECORATION, 192);
+  this.addSlice(SliceType.WINDOW, 192);
+  this.addSlice(SliceType.DECORATION, 192);
+  this.addSlice(SliceType.WINDOW, 192);
+  this.addSlice(SliceType.BACK, 192);
+}
+
+Walls.prototype.createTestSteppedWallSpan = function() {
+  this.addSlice(SliceType.FRONT, 192);
+  this.addSlice(SliceType.WINDOW, 192);
+  this.addSlice(SliceType.DECORATION, 192);
+  this.addSlice(SliceType.STEP, 256);
+  this.addSlice(SliceType.WINDOW, 256);
+  this.addSlice(SliceType.BACK, 256);
+}
+
+Walls.prototype.createTestGap = function() {
+  this.addSlice(SliceType.GAP);
+}
+
+Walls.prototype.createTestMap = function() {
+  for (var i = 0; i < 10; i++) {
+    this.createTestWallSpan();
+    this.createTestGap();
+    this.createTestSteppedWallSpan();
+    this.createTestGap();
+  }
 }
