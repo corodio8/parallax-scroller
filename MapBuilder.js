8daee9235a4d2c6ebe905177c1f6d5d44bc1e494
Justin Yang <corodio8@gmail.com>
2014-04-17 12:18:23 -0700

    Added steps to map randomizer

--- MapBuilder.js
+++ MapBuilder.js
@@ -13,7 +13,7 @@ MapBuilder.WALL_HEIGHTS = [
 
 MapBuilder.MAP_SIZE = 100;
 MapBuilder.MAX_WALL_SIZE = 10;
-MapBuilder.MIX_WALL_SIZE = 2;
+MapBuilder.MIN_WALL_SIZE = 3;
 MapBuilder.MAX_GAP_SIZE = 3;
 
 MapBuilder.prototype.createMap = function() {
@@ -24,19 +24,30 @@ MapBuilder.prototype.createMap = function() {
     var heightIndex = Math.floor(Math.random() * 5)
 
     if (slicesLeft < MapBuilder.MAX_WALL_SIZE + this.MAX_GAP_SIZE) {
+      //no stepped wall for final wall
       this.createWallSpan(heightIndex, slicesLeft, false, true);
 
       slicesLeft = 0;
     } else {
       var wallLength = Math.ceil(Math.random() * MapBuilder.MAX_WALL_SIZE);
-      if (wallLength < 2) { wallLength = 2};
-      this.createWallSpan(heightIndex, wallLength); 
+      if (wallLength < MapBuilder.MIN_WALL_SIZE) { wallLength = MapBuilder.MIN_WALL_SIZE};
+      
+      if (Math.random() * 2 > 1) {
+        this.createWallSpan(heightIndex, wallLength); 
+      } else {
+        spanBLength = Math.ceil(Math.random() * wallLength - 1 )
+        //spanBLength accounts for step and back so must be at least 2
+        if (spanBLength < 2) { spanBLength = 2 }
+        spanALength = wallLength - spanBLength;
+
+        this.createSteppedWallSpan(heightIndex, spanALength, spanBLength);
+      }
 
       slicesLeft = slicesLeft - wallLength;
 
       var gapLength = Math.ceil(Math.random() * MapBuilder.MAX_GAP_SIZE);
-      this.createGap(gapLength);
 
+      this.createGap(gapLength);
       slicesLeft = slicesLeft - gapLength;
 
     }
